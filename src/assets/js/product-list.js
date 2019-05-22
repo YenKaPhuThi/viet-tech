7a019da6692ef801ba6858aaa6823b03f59c41bd
YenKaPhuThi <yenkaphu96pnv@gmail.com>
2019-05-22 07:53:59 +0700

    config(viet-tech): struture viet-tech

---
+++ src/assets/js/product-list.js
@@ -0,0 +1,50 @@
+var productList = [
+    {
+        image: "",
+        name: "Black Genuine Leather Bags",
+        price: 23.20,
+        url: "",
+        like: "",
+        comment: ""
+    },
+    {
+        image: "",
+        name: " Fashion Red Leather Bags",
+        price: 20.16,
+        url: "",
+        like: "",
+        comment: ""
+    },
+    {
+        image: "",
+        name: " Fashion Genuine Leather Bags",
+        price: 30.14,
+        url: "",
+        like: "",
+        comment: ""
+    },
+    {
+        image: "",
+        name: " Simply Leather Bags",
+        price: 15.20,
+        url: "",
+        like: "",
+        comment: ""
+    },
+    {
+        image: "",
+        name: " New Fashion Leather Bags",
+        price: 25.20,
+        url: "",
+        like: "",
+        comment: ""
+    },
+    {
+        image: "",
+        name: " Single Hand Models Bags",
+        price: 17.6,
+        url: "",
+        like: "",
+        comment: ""
+    }
+]
